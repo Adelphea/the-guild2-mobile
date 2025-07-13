5a853519ab075f9c545f76c14b5d103a8aaff428
Adelphea <loys.scherrer@gmail.com>
2025-07-13 13:31:18 +0200

    Ajout d’un script principal "main_scene.gd"

---
+++ scenes/main_scene.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+# Script de base pour la scène principale
+# Tu pourras y ajouter la météo, l’heure, les événements ou les PNJ plus tard.
+
+func _ready():
+	print("Scène principale chargée")
